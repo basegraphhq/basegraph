3c9dc1152a610fc23020732de4c92a0b889357ec
Nithin SJ <thenithinraj@gmail.com>
2025-08-20 01:03:29 +0530

    chore: remove landing page project

--- codegraph/main.go
+++ codegraph/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/humanbeeng/lepo/prototypes/codegraph/extract/golang"
 	"github.com/humanbeeng/lepo/prototypes/codegraph/process"
 )
@@ -9,4 +11,5 @@ func main() {
 	// TODO : Remove extract interface and pass cmd line args for dir and pkg
 	e := golang.NewGoExtractor()
 	process.Orchestrate(e)
+	fmt.Println("Code graph extraction completed successfully.")
 }
