0c9f0f165221bba2a1e4c7aaf1f40a4b66eea2da
Nithin SJ <nithinsj@basegraph.app>
2025-12-07 12:35:17 +0530

    feat: init gin

--- relay/cmd/relay/main.go
+++ relay/cmd/relay/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Relay is running")
+	g := gin.Default()
+
+	if os.Getenv("RAILWAY_ENVIRONMENT_NAME") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	} else {
+		gin.SetMode(gin.DebugMode)
+	}
+
+	g.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+	slog.Info("Relay is running on port 8080")
+	g.Run(":8080")
 }
